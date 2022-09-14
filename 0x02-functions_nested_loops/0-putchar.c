dd5081df193b05ea9f37aa23dba327e03956cd8e
Amoszino1 <oyelekeamos744@gmail.com>
2022-09-14 05:56:49 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,14 @@
-#include<stdio.h>
-#include"main.h"
+#include "main.h"
+
 /**
- * main- Main entry*
- * description: Functions protoypes*
- * Return: Always 0 (success)
+ * main - print _putchar
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
-{{_putchar('_');
+{
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
@@ -14,5 +16,6 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	_putchar('\n'); }
-	return (0); }
+	_putchar('\n');
+	return (0);
+}
