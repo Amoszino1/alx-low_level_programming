2ea192d2ab1610e8d8346045c59e75e9232aec1b
Amoszino1 <oyelekeamos744@gmail.com>
2022-09-22 05:02:50 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,27 +1,21 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * _strncat - function that concatenates two strings. it will use
- * at most n bytes from src. src does not need to be null-terminated.
- * @dest: pointer to destination string.
- * @src: pointer to source string.
- * @n: number of bytes to be concatinated.
- * Return: pointer to destination string.
+ * _strncat - Concatenates two strings using at most
+ *            an inputted number of bytes from src.
+ * @dest: The string to be appended upon.
+ * @src: The string to be appended to dest.
+ * @n: The number of bytes from src to be appended to dest.
+ *
+ * Return: A pointer to the resulting string dest.
  */
-char *_strcat(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
-int length, j;
-/* j is a counter for n bytes of src to be concatenated */
-/* length = length of destination string */
-length = 0;
-while (dest[length] != '\0')
-{
-length++;
-}
-for (j = 0; j < n && src[j] != '\0'; j++, length++)
-{
-dest[length] = src[j];
-}
-dest[length] = '\0';
-return (dest);
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+	return (dest);
 }
